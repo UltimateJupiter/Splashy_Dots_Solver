844456b699c50b05f653dd6c76ebf0336a6f61d5
UltimateJupiter <jupiter_zhu_007@126.com>
2018-01-07 12:35:06 +0800

    1-7Update

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ import datetime
 
 for x in range(3, 9):
     # start_time = datetime.datetime.now()
+
     task_matrix = image_to_matrix('tasks/' + str(x) + '.png')
     start_time = datetime.datetime.now()
-    print('Solution:', strat(task_matrix)[1])
+
+    result = strat(task_matrix)
+    print('Solution:', result[1])
+    
     end_time = datetime.datetime.now()
     interval = end_time - start_time
     print('Time_Cost: ' + str(float(str(interval)[6:]) * 1000) + 'ms')
